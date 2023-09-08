75b5036bca9225567c331abafd33be2c3b4c5e81
Brian <wamwangimathenge@gmail.com>
2023-09-08 14:58:36 +0300

    RUn the tests

--- addFive.test.js
+++ addFive.test.js
@@ -1,6 +1,6 @@
-const { default: test } = require("jest-runner");
+const { default: TestRunner } = require("jest-runner");
 const addFive = require(`./addFive`);
 
 test('returns the number plus 5', () => {
-    expect(addFive(1).toBe(6))
+    expect(addFive(1)).toBe(6)
 })
